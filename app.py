c6e2d3852ce1718b373348758c5e01613dfe1158
Peter Bengtsson <mail@peterbe.com>
2020-05-04 21:37:27 -0400

    add black and flake8

--- app.py
+++ app.py
@@ -17,14 +17,13 @@ import peewee
 # Models
 
 db = peewee.PostgresqlDatabase(
-    os.environ.get('PREMAILER_DATABASE_NAME', 'premailer'),
-    user=os.environ.get('PREMAILER_DATABASE_USER', 'peterbe'),
-    password=os.environ.get('PREMAILER_DATABASE_PASSWORD', 'secret'),
-    host=os.environ.get('PREMAILER_DATABASE_HOST', 'localhost'),
-    port=int(os.environ.get('PREMAILER_DATABASE_PORT', '5432')),
-
+    os.environ.get("PREMAILER_DATABASE_NAME", "premailer"),
+    user=os.environ.get("PREMAILER_DATABASE_USER", "peterbe"),
+    password=os.environ.get("PREMAILER_DATABASE_PASSWORD", "secret"),
+    host=os.environ.get("PREMAILER_DATABASE_HOST", "localhost"),
+    port=int(os.environ.get("PREMAILER_DATABASE_PORT", "5432")),
     # http://docs.peewee-orm.com/en/latest/peewee/database.html#using-autoconnect
-    autoconnect=False
+    autoconnect=False,
 )
 
 
@@ -41,18 +40,14 @@ class Post(peewee.Model):
     class Meta:
         database = db
 
+
 db.connect()
 db.create_tables([Post])
 
 
 def insert_post(html, options, url=None, user_agent=None):
     db.connect(reuse_if_open=True)
-    row = Post(
-        html=html,
-        options=json.dumps(options),
-        url=url,
-        user_agent=user_agent
-    )
+    row = Post(html=html, options=json.dumps(options), url=url, user_agent=user_agent)
     row.save()
     return row
 
@@ -66,10 +61,11 @@ def update_post(row, duration, error=None, result=None):
         row.result = result
     row.save()
 
+
 # /Models
 
 
-CORS_ORIGIN = os.environ.get('CORS_ORIGIN', 'premailer.io')
+CORS_ORIGIN = os.environ.get("CORS_ORIGIN", "premailer.io")
 
 
 @contextlib.contextmanager
@@ -82,7 +78,6 @@ def redirect_streams(stdout, stderr):
 
 
 class TransformResource:
-
     def on_post(self, req, resp):
         # req.stream corresponds to the WSGI wsgi.input environ variable,
         # and allows you to read bytes from the request body.
@@ -94,39 +89,37 @@ class TransformResource:
 
         body = req.stream.read()
         if not body:
-            raise falcon.HTTPBadRequest('Empty request body',
-                                        'A valid JSON document is required.')
-        body = json.loads(body.decode('utf-8'))
+            raise falcon.HTTPBadRequest(
+                "Empty request body", "A valid JSON document is required."
+            )
+        body = json.loads(body.decode("utf-8"))
 
         url = None
-        if body.get('url'):
-            url = body.pop('url')
+        if body.get("url"):
+            url = body.pop("url")
             html = self._download_url(url)
-            if 'html' in body:
-                body.pop('html')
+            if "html" in body:
+                body.pop("html")
 
         else:
-            html = body.pop('html')
-            if 'url' in body:
-                body.pop('url')
+            html = body.pop("html")
+            if "url" in body:
+                body.pop("url")
         options = body
 
         row = insert_post(html, options, url=url, user_agent=req.user_agent)
-        pretty_print = options.pop('pretty_print', True)
+        pretty_print = options.pop("pretty_print", True)
 
         mylog = StringIO()
         logging_handler = logging.StreamHandler(mylog)
-        logging_handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
-        options['cssutils_logging_handler'] = logging_handler
-        options['cssutils_logging_level'] = logging.WARNING
+        logging_handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
+        options["cssutils_logging_handler"] = logging_handler
+        options["cssutils_logging_level"] = logging.WARNING
 
         try:
             p = premailer.Premailer(html, **options)
         except TypeError as exp:
-            raise falcon.HTTPBadRequest(
-                'Invalid options to premailer',
-                str(exp)
-            )
+            raise falcon.HTTPBadRequest("Invalid options to premailer", str(exp))
         error = None
         warnings = None
         t0 = time.time()
@@ -134,7 +127,7 @@ class TransformResource:
             result = p.transform(pretty_print=pretty_print)
         except Exception:
             exc_type, exc_value, __ = sys.exc_info()
-            error = '{} ({})'.format(exc_type.__name__, exc_value)
+            error = "{} ({})".format(exc_type.__name__, exc_value)
 
         warnings = mylog.getvalue()
         t1 = time.time()
@@ -145,24 +138,19 @@ class TransformResource:
             update_post(row, t1 - t0, error=error)
 
         resp.status = falcon.HTTP_200
-        resp.set_header('Access-Control-Allow-Origin', CORS_ORIGIN)
-        resp.set_header('Content-Type', 'application/json')
+        resp.set_header("Access-Control-Allow-Origin", CORS_ORIGIN)
+        resp.set_header("Content-Type", "application/json")
         took = (t1 - t0) * 1000
         if error:
-            resp.body = json.dumps({
-              'errors': [error],
-              'took': took,
-            }, indent=2)
+            resp.body = json.dumps({"errors": [error], "took": took}, indent=2)
         else:
-            resp.body = json.dumps({
-              'html': result,
-              'took': took,
-              'warnings': warnings,
-            }, indent=2)
+            resp.body = json.dumps(
+                {"html": result, "took": took, "warnings": warnings}, indent=2
+            )
 
         # cleaning up
         del p
-        #mylog.close()
+        # mylog.close()
 
     def _download_url(self, url):
         return requests.get(url).text
@@ -171,9 +159,9 @@ class TransformResource:
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
 
-app.add_route('/api/transform', TransformResource())
+app.add_route("/api/transform", TransformResource())
 
 
-if __name__ == '__main__':
-    httpd = simple_server.make_server('127.0.0.1', 5000, app)
+if __name__ == "__main__":
+    httpd = simple_server.make_server("127.0.0.1", 5000, app)
     httpd.serve_forever()
